2cd798339d5ce2233581275d3c3f510ad523be46
daniel-winner <34260767@qq.com>
2019-12-11 09:31:31 +0800

    添加定时任务

--- src/main/java/com/js/sas/SasApplication.java
+++ src/main/java/com/js/sas/SasApplication.java
@@ -2,9 +2,11 @@ package com.js.sas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
+@EnableScheduling
 @SpringBootApplication
 public class SasApplication {
 
